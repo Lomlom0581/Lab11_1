b3971eb88c44986d25eeed576561d4fe5dde3f00
Антон Еремеев <kzanton_81@mail.ru>
2021-12-25 22:08:18 +0300

    Добавьте файлы проекта.

---
+++ Lab11_1/LinearEquation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_1
+{
+    class LinearEquation
+    {
+        public double k;
+        public double b;
+                
+        public double Root() => -b / k;
+    }
+}
---
+++ Lab11_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Привет! Программа вычисляет корень линейного уравнения вида k*x+b=0");
+            Console.Write("Введите коэффициент k:");
+            double k = int.Parse(Console.ReadLine());
+            Console.Write("Введите коэффициент b:");
+            double b = int.Parse(Console.ReadLine());
+
+            LinearEquation eq = new LinearEquation();
+            eq.k = k;
+            eq.b = b;
+           
+            if (double.IsInfinity(eq.Root()))
+            {
+                Console.WriteLine($"Корней нет");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Корень: {eq.Root()}");
+            Console.ReadKey();
+        }
+    }
+}
